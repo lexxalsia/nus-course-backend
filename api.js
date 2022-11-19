e52d7360126669e3ca8e6ac4cc9682fadefb200c
hweeping-goh <hweeping.goh2@gmail.com>
2022-11-19 14:44:27 +0800

    added "Dixant's version"

--- api.js
+++ api.js
@@ -1,3 +1,5 @@
+// SYK's version
+
 const express = require("express");
 const data = require("./mock_data");
 
@@ -32,6 +34,56 @@ router.post("/users", (req, resp) => {
   data.add_user(user);
 
   resp.status(201).send(`User added`);
+});
+
+module.exports = { router };
+
+// Dixant's version
+
+const express = require("express");
+const data = require("./mock_data");
+
+// export let router = express.Router();
+
+let router = express.Router();
+
+router.get("/sum", (request, response) => {
+  response.send(
+    `Sum is ${parseFloat(request.query.n1) + parseFloat(request.query.n2)}`
+  );
+});
+
+router.get("/product", (request, response) => {
+  response.send(
+    `Product is ${parseFloat(request.query.n1) * parseFloat(request.query.n2)}`
+  );
+});
+
+router.get("/users/all", (request, response) => {
+  let users = data.get_all_users();
+  response.send(users);
+});
+
+router.get("/users/by-id", (request, response) => {
+  let user = data.get_user_by_user_id(parseInt(request.query.user_id));
+  response.send(user);
+});  
+
+router.post("/users/add", (request, response) => {
+    let user = {
+      first_name: request.body.first_name,
+      last_name: request.body.last_name,
+      email: request.body.email,
+      user_id: request.body.user_id,
+      phone: request.body.phone,
+      plan_id: request.body.plan_id,
+      signup_date: request.body.signup_date,
+    };
+  //   data.add_user(user);
+  //   response.send("User added successfully!");
+  // });
+  data.add_user(user);
+  response.status(201).send("User added successfully!");
 });
 
 module.exports = { router };
