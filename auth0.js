b264c2b03fd1185528e89729e50d67ec76b53ab1
lexxalsia <sykang_8@msn.com>
2022-11-21 09:39:05 +0800

    Add Auth0 code

---
+++ auth0.js
@@ -0,0 +1,26 @@
+const express = require("express");
+require("dotenv").config();
+
+let authRouter = express.Router();
+
+const { auth, requiresAuth } = require("express-openid-connect");
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL,
+};
+
+// req.isAuthenticated is provided from the auth router
+authRouter.get("/", (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out");
+});
+
+authRouter.get("/profile", requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
+
+module.exports = { auth, config, authRouter };
--- main.js
+++ main.js
@@ -1,30 +1,20 @@
-// import express from "express";
-
-// let app = express();
-
-// app.get("/", (req, res) => {
-//   res.send("Hello world!");
-// });
-
-// app.listen(3000, (errors) => {
-//   if (errors) {
-//     console.log(errors);
-//   } else {
-//     console.log("Listening on port 3000");
-//   }
-// });
-
 const express = require("express");
-const apis = require("./api");
+require("dotenv").config();
+
+const { router } = require("./api");
+const { auth, config, authRouter } = require("./auth0");
 
 let app = express();
 app.use(express.json());
-app.use(apis.router);
+app.use(router);
 
-app.listen(3000, (errors) => {
+app.use(auth(config));
+app.use(authRouter);
+
+app.listen(process.env.PORT, (errors) => {
   if (errors) {
     console.log(errors);
   } else {
-    console.log("Listening on port 3000");
+    console.log(`Listening on port ${process.env.PORT}`);
   }
 });
